3140320406485c85c11ee9dd22fa7eb3993bfe36
root <root@sal-sdv3.research.intel-research.net>
2016-07-19 16:39:02 -0700

    More benchmarks.

---
+++ l3_bw.cpp
@@ -0,0 +1,45 @@
+#include "lz4_mt.h"
+#include <sched.h>
+
+static __inline__ unsigned long long rdtsc(void)
+{
+  unsigned hi, lo;
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+  return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+}
+
+
+static unsigned long get_current_rtc()
+{
+  struct timeval tm;
+  gettimeofday(&tm, NULL);
+  return tm.tv_sec*1000000 + tm.tv_usec;
+}
+
+#define SIZE (20*1024*1024UL)
+#define LOOPS 200UL
+
+main()
+{
+
+  volatile char *space = (volatile char *)map_anon_memory(SIZE); // 20 MB
+  register char total = 0;
+  cpu_set_t my_set;        
+  CPU_ZERO(&my_set);       
+  CPU_SET(2, &my_set);     
+  sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
+  // Bring into L3 cache
+  for(unsigned long index = 0; index < SIZE; index++) {
+    total = total + space[index];
+  }
+  unsigned long start = rdtsc();
+  for(int loops=0;loops < LOOPS;loops++) {
+    for(unsigned long index = 0; index < SIZE; index++) {
+      total = total + space[index];
+    }
+  }
+  unsigned long total_time = rdtsc() - start;
+  unsigned long total_bytes = SIZE*LOOPS;
+  double bw = ((double)total_bytes)/(total_time);
+  printf("BW = %lf B/cycle\n", bw);
+}

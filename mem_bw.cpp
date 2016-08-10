e076b727a3fdcf44fc29c9c46e9a439feb88be41
root <root@sal-sdv3.research.intel-research.net>
2016-08-10 09:56:55 -0700

    Update mem bw benchmark.

--- mem_bw.cpp
+++ mem_bw.cpp
@@ -1,5 +1,23 @@
-#include "lz4_mt.h"
 #include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+void *map_anon_memory(unsigned long size)
+{
+  void *space = mmap(NULL, size > 0 ? size : 4096,
+                     PROT_READ | PROT_WRITE,
+                     MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+  if (space == MAP_FAILED) {
+    printf("map output failed\n");
+    exit(-1);
+  }
+  if (mlock(space, size) < 0) {
+    printf("mlock output failed\n");
+    exit(-1);
+  }
+  return space;
+}
 
 static __inline__ unsigned long long rdtsc(void)
 {
@@ -8,16 +26,17 @@ static __inline__ unsigned long long rdtsc(void)
   return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
 
+// Mem
+//#define SIZE (80*1024*1024UL)
+//#define LOOPS 200UL
 
-static unsigned long get_current_rtc()
-{
-  struct timeval tm;
-  gettimeofday(&tm, NULL);
-  return tm.tv_sec*1000000 + tm.tv_usec;
-}
+// L3
+//#define SIZE (10*1024*1024UL)
+//#define LOOPS 800UL
 
-#define SIZE (20*1024*1024UL)
-#define LOOPS 200UL
+// L2
+#define SIZE (128*1024UL)
+#define LOOPS 128000UL
 
 main()
 {
@@ -34,7 +53,7 @@ main()
   }
   unsigned long start = rdtsc();
   for(int loops=0;loops < LOOPS;loops++) {
-    for(unsigned long index = 0; index < SIZE; index++) {
+    for(unsigned long index = 0; index < SIZE; index+=64) {
       total = total + space[index];
     }
   }
